83ca3fe6739f2a6c4b8d7c3fc1a26a4b14ff3704
Kaushik Verma <kaushikverma321@gmail.com>
2024-01-10 23:01:21 +0530

    392 Done

--- LeetCode/1299. Replace Elements with Greatest Element on Right Side/Replace-Elements-with-Greatest-Element-on-Right-Side.cpp
+++ LeetCode/1299. Replace Elements with Greatest Element on Right Side/Replace-Elements-with-Greatest-Element-on-Right-Side.cpp
@@ -1,15 +1,34 @@
-class Solution {
-public:
-    vector<int> replaceElements(vector<int>& arr) {
-        int n =arr.size();
-        int max = arr[n-1];
-        arr[n-1] = -1;
-        for(int i=n-2;i>=0;i--){
-            int temp = arr[i];
-            arr[i]=max;
-            if(max<temp)
-                max= temp;
-        }
-        return arr;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int display(vector<int> &arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
     }
-};
+    cout << endl;
+}
+int replaceElements_BrutalForce(vector<int> &arr)
+{
+    vector<int> arr1;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        int a = -1;
+        for (int j = i + 1; j < arr.size(); j++)
+        {
+            a = max(a, arr[j]);
+        }
+        arr1.push_back(a);
+    }
+    display(arr1);
+}
+int replaceElements_Optimise(vector<int> &arr){
+    int max = -1;
+}
+int main()
+{
+    vector<int> arr = {17, 18, 5, 4, 6, 1};
+    replaceElements_BrutalForce(arr);
+}
